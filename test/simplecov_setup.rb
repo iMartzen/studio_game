5532bf7266968a83665a0508af3c54fe249f4824
Martzen <martzen@hackerone.com>
2025-04-06 16:34:21 +0200

    update in workflow for failure when coverage is < 80%

--- test/simplecov_setup.rb
+++ test/simplecov_setup.rb
@@ -3,4 +3,5 @@ SimpleCov.start do
   add_filter "/test/"  # Exclude test files from coverage report
   track_files "lib/**/*.rb"  # Track all Ruby files in lib directory
   enable_coverage :branch  # Track branch coverage as well as line coverage
+  minimum_coverage 80  # Set minimum coverage threshold to 80%
 end
